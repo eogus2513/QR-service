2b5752a5e25597a4596c3fc76150bc6c1e912855
정대현 <eogus_2513@dsm.hs.kr>
2022-10-20 11:06:55 +0900

    cors

---
+++ src/main/kotlin/com/example/qr/security/CorsConfig.kt
@@ -0,0 +1,16 @@
+package com.example.qr.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig: WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowedOrigins("*")
+    }
+}
